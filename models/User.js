fb195c17ba04ea60a6aef898ce001f77a2acb511
Mitchell Frey <mfcodingbc@gmail.com>
2022-06-12 19:03:15 -0400

    initial setup, user model created

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'You need a username to create an account.',
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'You must have a valid email to create an account.',
+      match: [/.+\@.+\..+/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
